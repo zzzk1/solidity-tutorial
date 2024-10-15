6380b0033168c0ec71498c8b6e57c6bceccd0237
zzzk1 <madzhou1@gmail.com>
2024-10-15 15:01:43 +0800

    task: ether-units

---
+++ ether-units.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    bool public isOneWei = (oneWei == 1);
+
+    uint256 public oneGwei = 1 gwei;
+    bool public isOneGWei = (oneGwei == 1e9);
+
+    uint256 public oneEther = 1 ether;
+    bool public isOneEther = (oneEther == 1e18);
+}
