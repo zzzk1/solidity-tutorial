7282645fd71cfd0ca0b116ec936993c7a95b332e
zzzk1 <madzhou1@gmail.com>
2024-10-16 14:59:50 +0800

    task: constructor

---
+++ constructor.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract X {
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
+
+contract Y {
+    string public text;
+
+    constructor(string memory _text) {
+        text = _text;
+    }
+}
+
+contract B is X("Input to X"), Y("Input to Y") {}
+
+contract C is X, Y {
+    constructor(string memory _name, string memory _text) X(_name) Y(_text) {}
+}
+
+contract D is X, Y {
+    constructor() X("X was called") Y("Y was called") {}
+}
