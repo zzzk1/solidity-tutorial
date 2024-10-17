0d9ec362730f5c66deb51aeef13211e50cff8152
zzzk1 <madzhou1@gmail.com>
2024-10-17 14:19:54 +0800

    task: fallback

---
+++ fallback.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Fallback {
+    event Log(string func, uint256 gas);
+
+    fallback() external payable {
+        emit Log("fallback", gasleft());
+    }
+
+    receive() external payable {
+        emit Log("receive", gasleft());
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
+
+contract SendToFallback {
+    function transferToFallback(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function callFallback(address payable _to) public payable {
+        (bool sent, ) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
